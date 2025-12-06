60e7f8d8225319dbd432c695551a186670989cbb
Emilia Alves <emiliaalves.tarffie@gmail.com>
2025-12-06 20:52:13 -0300

    chore: cleaning up and formating code

--- services/backend/src/index.ts
+++ services/backend/src/index.ts
@@ -6,34 +6,29 @@ import { Server } from "socket.io";
 import connectDB from "./config/database.js"; // DB import
 import "dotenv/config"; // Loads .env
 import authRoutes from "./routes/auth";
-import { z } from "zod";
 import { errorHandler } from './middleware/errorHandler.ts'
 
 // Connect DB on startup
 connectDB();
-
 // App setup
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
-});
 
-// Bun can't handle cookie parsing by itself, this solves
-app.use(cookieParser());
-// Enabling cross origin cors so our frontend can fetch the api
-app.use(cors({ origin: "http://localhost:5173" }));
-// built-in middleware to parse incoming requets as json
-app.use(express.json());
+app.use(cookieParser()); // Bun can't handle cookie parsing by itself, this solves
+app.use(cors({ origin: `${process.env.FRONTEND_ORIGIN}` })); // Enabling cross origin cors so our frontend can fetch the api
+app.use(express.json()); // built-in middleware to parse incoming requets as json
 app.use("/api/auth", authRoutes);
-
 app.use(errorHandler);
 
-//io.on("connection", (socket) => {
-//  console.log("User connected:", socket.id);
-//  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
-//  // TODO: Emit 'user:online' or handle auth handshake
-//});
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: `${process.env.FRONTEND_ORIGIN}` },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+  // TODO: Emit 'user:online' or handle auth handshake
+});
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server on ${PORT}`));
--- services/frontend/src/pages/Home.tsx
+++ services/frontend/src/pages/Home.tsx
@@ -1,31 +1,23 @@
-import { useState } from "react";
-import { HomeButton } from "../components/HomeButton";
-import { AuthForm } from "../components/AuthForm";
-import { login, register } from "../utils/authUtils";
+import { useState } from 'react';
+import { HomeButton } from '../components/HomeButton';
+import { AuthForm } from '../components/AuthForm';
+import { login, register } from '../utils/authUtils';
 
 /**
  * Main page component that should be rendered in case user isn't authenticated
  * Sort of a Landing page, if you will
  */
 export const Home = () => {
-  const [loginOrRegister, setLoginOrRegister] = useState<"Login" | "Register">(
-    "Login",
-  );
+  const [loginOrRegister, setLoginOrRegister] = useState<'Login' | 'Register'>('Login');
 
   return (
     <>
     <h1> Hello, world guest </h1>
     <div className="buttonRow">
-    <HomeButton
-    index={"loginButton"}
-    callbackFn={() => setLoginOrRegister("Login")}
-    >
+    <HomeButton index={'loginButton'} callbackFn={() => setLoginOrRegister('Login')}>
     Login
     </HomeButton>
-    <HomeButton
-    index={"registerButton"}
-    callbackFn={() => setLoginOrRegister("Register")}
-    >
+    <HomeButton index={'registerButton'} callbackFn={() => setLoginOrRegister('Register')}>
     Register
     </HomeButton>
     <p> {loginOrRegister} </p>
