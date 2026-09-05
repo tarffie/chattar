84e6aea69e4f45953d6d10ee4d417b4c7e683845
Emilia Alves <emiliaalves.tarffie@gmail.com>
2026-09-04 23:34:28 -0300

    feat?: initial implementation for backend testing

--- apps/backend/tests/userService.test.ts
+++ apps/backend/tests/userService.test.ts
@@ -1,22 +1,67 @@
-import { test, expect } from "vitest";
-import {
-  registerUser,
-  getUserById,
-  loginUser,
-  updateUserKeys,
-} from "../src/services/userService.ts";
+import Bun from 'bun';
+import { test, expect } from 'vitest';
+import { registerUser, loginUser, updateUserKeys } from '../src/services/userService.ts';
 
-import User from "../src/models/User";
+import User from '../src/models/User';
 
-// define user objects to test
+const { publicKey } = await (async () => {
+  const keyPair = await crypto.subtle.generateKey(
+    {
+      name: 'RSA-OAEP',
+      modulusLength: 2048,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: 'SHA-256',
+    },
+    true, // extractable
+    ['encrypt', 'decrypt'],
+  );
 
-// valid user
-const user1 = {
-  username: "tarffie",
-  password: "testPassword",
-  email: "exampleEmail@example.com",
-};
+  const publicKeyBuffer = await crypto.subtle.exportKey('spki', keyPair.publicKey);
+  const publicKeyBase64 = btoa(String.fromCharCode(...new Uint8Array(publicKeyBuffer)));
 
-//test("should say hi", () => {
-//  expect(registerUser(user1)).toBe({success: true, data: });
-//});
+  return {
+    publicKey: publicKeyBase64,
+  };
+})();
+
+const successUser = new User({
+  username: 'tarffie',
+  password: 'testPassword',
+  email: 'exampleEmail@example.com',
+  publicKey: publicKey,
+});
+
+console.log(publicKey);
+
+const { password: t, ...safeUser } = successUser.toJSON();
+
+test('REGISTER: should return success when registering a USER', async () => {
+  expect(
+    await registerUser({
+      username: 'tarffie',
+      password: 'testPassword',
+      email: 'exampleEmail@example.com',
+      publicKey: publicKey,
+    }),
+  ).toBe({ success: true, data: safeUser });
+});
+
+test('LOGIN: should return success when authenticating a USER', async () => {
+  expect(
+    await loginUser({
+      email: 'exampleEmail@example.com',
+      password: 'testPassword',
+    }),
+  ).toBe({ success: true, data: typeof User });
+});
+
+test('REGISTER: should return failure when registering a USER', async () => {
+  expect(
+    await registerUser({
+      username: 'tarffie',
+      password: 'testPassword',
+      email: 'exampleEmail@example.com',
+      publicKey: publicKey,
+    }),
+  ).toBe(typeof Error);
+});
